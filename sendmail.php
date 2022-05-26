61c85e7ad53966d5bde634817287ea5215754cdb
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-05-26 18:02:36 -0300

    Update

--- sendmail.php
+++ sendmail.php
@@ -1,78 +1,86 @@
 <?php
 
-$data_reclamacao=$_POST["data_reclamacao"];
+use function Composer\Autoload\includeFile;
+
 $nome=$_POST["nome"];
 $email=$_POST["email"];
 $data_incidente=$_POST["data_incidente"];
 $local=$_POST["local"];
 $reclamacao=$_POST["reclamacao"];
 $solucao=$_POST["resultado"];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
 
-$mensagem= 'Esta mensagem foi enviada através do formulário<br><br>';
-$mensagem.='<b>Nome: </b>'.$nome.'<br>';
-$mensagem.='<b>E-Mail: </b> '.$email.'<br>';
-$mensagem.='<b>Data incidente: </b> '.$data_incidente.'<br>';
-$mensagem.='<b>Local do incidente: </b> '. $local.'<br>';
-$mensagem.='<b>Reclamação: </b> '.$reclamacao.'<br>';
-$mensagem.='<b>Sugestão de solução: </b> '.$solucao.'<br>';
+// Compo E-mail
 
-require '/xampp/htdocs/HTML-Forms/vendor/autoload.php';
+$htmlContent = file_get_contents("corpomail.html");
+$htmlContent = preg_replace('[{{nome}}]', $nome, $htmlContent);
+$htmlContent = preg_replace('[{{email}}]', $email, $htmlContent);
+$htmlContent = preg_replace('[{{data_incidente}}]', $data_incidente, $htmlContent);
+$htmlContent = preg_replace('[{{local}}]', $local, $htmlContent);
+$htmlContent = preg_replace('[{{reclamacao}}]', $reclamacao, $htmlContent);
+$htmlContent = preg_replace('[{{solucao}}]', $solucao, $htmlContent);
+$htmlContent = preg_replace('[{{data_envio}}]', $data_envio, $htmlContent);
+$htmlContent = preg_replace('[{{hora_envio}}]', $hora_envio, $htmlContent);
+
 
 // Instância da classe
 
-$mail = new PHPMailer\PHPMailer\PHPMailer();
+require '/xampp/htdocs/HTML-Forms/vendor/autoload.php';
+
+$mailer = new PHPMailer\PHPMailer\PHPMailer();
 
 // Configurações do servidor
 
-    $mail->isSMTP();        //Define o uso de SMTP no envio
+    $mailer->isSMTP();        //Define o uso de SMTP no envio
 
-    $mail->SMTPAuth     = true; //Habilita a autenticação SMTP
+    $mailer->SMTPAuth     = true; //Habilita a autenticação SMTP
 
-    $mail->Username     = 'contato@gpcabling.com.br';
+    $mailer->Username     = 'contato@gpcabling.com.br';
 
-    $mail->Password     = '*Copopp67';
+    $mailer->Password     = '*Copopp67';
 
-    $mail->CharSet = 'UTF-8';    //DEFINE O CHARSET UTILIZADO
+    $mailer->CharSet = 'UTF-8';    //DEFINE O CHARSET UTILIZADO
 
     // Criptografia do envio SSL também é aceito
 
-    $mail->SMTPSecure = 'tls';
+    $mailer->SMTPSecure = 'tls';
 
     // Informações específicadas pelo Google
 
-    $mail->Host = 'smtp.office365.com';
+    $mailer->Host = 'smtp.office365.com';
 
-    $mail->Port = 587;
+    $mailer->Port = 587;
 
     // Define o remetente
 
-    $mail->setFrom('contato@gpcabling.com.br', 'TI GPCabling');
+    $mailer->setFrom('contato@gpcabling.com.br', 'TI GPCabling');
 
     // Define o destinatário
 
-    $mail->addAddress('richard@gpcabling.com.br', 'Richard');
-    $mail->addAddress('lucas@gpcabling.com.br', 'Lucas Vilar');
+    //$mailer->addAddress('richard@gpcabling.com.br', 'Richard');
+    $mailer->addAddress('lucas@gpcabling.com.br', 'Lucas Vilar');
 
     // Conteúdo da mensagem
 
-    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+    $mailer->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
 
-    $mail->Subject   = utf8_decode('Formulário de Reclamação');
+    $mailer->Subject   = utf8_decode('Formulário de Reclamação');
 
-    $mail->Body      = $mensagem;
+    $mailer->Debugoutput = 'html';
 
-    $mail->SMTPDebug = 1;
+    $mailer->SMTPDebug   = 1;
 
-    $mail->Debugoutput = 'html';
+    $mailer->Body        = $htmlContent;
 
-    $mail->setLanguage('pt');
+    $mailer->setLanguage('pt');
 
     // Enviar
 
-    if(!$mail->Send()) {
+    if(!$mailer->Send()) {
         echo "Erro ao enviar o E-Mail";
     }else{
         echo "E-Mail enviado com sucesso!";
     }
-
+    
 ?>
